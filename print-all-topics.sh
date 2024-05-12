23f3e184a2f3416db2cc03b884d14212e06ae4b6
Kirill Rekhov <krekhov.dev@gmail.com>
2024-05-12 19:44:47 +0300

    Sun May 12 07:44:50 PM MSK 2024

---
+++ print-all-topics.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Show all topics from all *.txt files
+# Documentation files have for example:
+# Темы:
+# * [a-z]
+# * [a-z]
+# ...
+#
+
+for f in $(find . -type f -name "*.txt" -not -empty); do
+	if grep 'Темы:' $f >/dev/null; then
+		echo -e "\e[96m*** ----- $f ----- ***\e[0m"
+		grep -Pzo 'Темы:\n\K\*.*\n(?:\*.*\n)*' --color $f
+	fi
+done
